03240abfcacda04152d95bd57c5f3c36b50e03ee
Prakhar <playingprakhar122@gmail.com>
2025-09-13 00:53:45 +0530

    creating food deleivery application

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+dotenv.config()
+const app = express();
+const port = process.env.PORT || 5000
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+});
